125490388dac1d3da8f80a770d547623cf1a69a0
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-10-18 09:35:10 +0200

    RecipeList working

---
+++ frontend/src/components/RecipeCard.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="card">
+    <!-- Recipe Image -->
+    <img :src="recipe.imagePath" alt="Recipe Image" class="recipe-image" />
+
+    <div class="details">
+      <!-- Recipe Title -->
+      <h3>{{ recipe.name }}</h3>
+
+      <!-- Recipe Info (Time, Protein, Kcal) -->
+      <div class="info">
+        <div class="info-item">
+          <i class="fa fa-clock"></i>
+          <span>{{ recipe.totalTime === "PT0S" ? "❓" : recipe.totalTime.substring(2).toLowerCase() }}</span>
+        </div>
+
+        <div class="info-item">
+          <i class="fa fa-dumbbell"></i>
+          <span>{{ recipe.macros.proteins }}g protein</span>
+        </div>
+
+        <div class="info-item">
+          <i class="fa fa-fire"></i>
+          <span>{{ calculateCalories(recipe.macros) }} kcal</span>
+        </div>
+      </div>
+
+      <!-- Recipe Link (Button) -->
+      <a :href="recipe.websiteUrl" class="cook-button">Kook dit recept</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['recipe'],
+  methods: {
+    calculateCalories(macros) {
+      return Math.round(4 * (macros.proteins + macros.carbs) + macros.fats * 9);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.recipe-image {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+.details {
+  padding: 1rem;
+  text-align: center;
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.8rem;
+  color: var(--color-heading);
+}
+
+.info {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.info-item {
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+}
+
+.info-item i {
+  font-size: 1rem;
+}
+
+.cook-button {
+  display: inline-block;
+  padding: 0.8rem 1.6rem;
+  background-color: #4caf50;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-weight: 600;
+}
+
+.cook-button:hover {
+  background-color: #45a049;
+}
+
+@media (min-width: 1024px) {
+  .card {
+    flex-direction: row;
+  }
+
+  .recipe-image {
+    width: 40%;
+    height: 100%;
+  }
+
+  .details {
+    text-align: left;
+    padding: 2rem;
+  }
+
+  .info {
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ frontend/src/components/RecipeList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="recipe-grid">
+      <RecipeCard 
+        v-for="recipe in recipes" 
+        :key="recipe.id" 
+        :recipe="recipe" />
+    </div>
+    <div class="recipe-grid-footer">
+      <button @click="loadMoreRecipes" v-if="lastEvaluatedId" class="load-more-button">Load More</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import RecipeCard from '@/components/RecipeCard.vue';
+
+export default {
+  components: {
+    RecipeCard
+  },
+  data() {
+    return {
+      recipes: [],
+      lastEvaluatedId: null
+    };
+  },
+  methods: {
+    fetchRecipes() {
+      let url = 'http://127.0.0.1:8080/recipes?pageSize=12';
+      if (this.lastEvaluatedId) {
+        url += `&lastEvaluatedId=${this.lastEvaluatedId}`;
+      }
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          this.recipes = [...this.recipes, ...data.recipes];
+          this.lastEvaluatedId = data.lastEvaluatedId;
+        })
+        .catch(error => {
+          console.error('Error fetching recipes:', error);
+        });
+    },
+    loadMoreRecipes() {
+      this.fetchRecipes();
+    }
+  },
+  created() {
+    this.fetchRecipes();
+  }
+};
+</script>
+
+<style scoped>
+.recipe-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 20px;
+}
+.load-more-button {
+  margin: 2rem;
+  padding: 0.8rem 1.6rem;
+  background-color: #4caf50;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-weight: 600;
+}
+.recipe-grid-footer {
+  display: flex;
+  justify-content: center;
+}
+</style>
