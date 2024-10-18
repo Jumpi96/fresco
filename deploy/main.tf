5bb1c14332e24e02bb24fa0e69a3bd9ba6c0a0f1
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-10-18 09:07:12 +0200

    deploy change to generate presigned

--- deploy/main.tf
+++ deploy/main.tf
@@ -114,7 +114,8 @@ resource "aws_iam_user_policy" "script_user_policy" {
         Action = [
           "s3:PutObject",
           "s3:GetObject",
-          "s3:DeleteObject"
+          "s3:DeleteObject",
+          "s3:ListBucket"
         ],
         Effect   = "Allow",
         Resource = "${aws_s3_bucket.recipe_storage.arn}/*"
