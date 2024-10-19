e66c038602582fa9685aea5ce0bdb0e13b6bdc5d
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-10-19 10:08:06 +0200

    bad name again

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "fresco-terraform-storage-bucket"
+    bucket = "fresco-terraform-state-bucket"
     key    = "terraform.tfstate"
     region = "eu-central-1"
     encrypt = true
