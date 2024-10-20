1fdfdbcd6fbe82158bf192bbd764786da9e59562
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-10-20 13:04:39 +0200

    using fresco_bucket_name

--- deploy/file_storage.tf
+++ deploy/file_storage.tf
@@ -47,3 +47,7 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls  = true
   restrict_public_buckets = true
 }
+
+output "fresco_bucket_name" {
+  value = aws_s3_bucket.recipe_storage.id
+}
