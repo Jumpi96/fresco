6710a2cfc71a2482c7ab2326420e4805e8b3cd8b
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-10-20 08:59:55 +0200

    openai help

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -9,12 +9,6 @@ resource "aws_elastic_beanstalk_environment" "fresco_backend_env" {
   solution_stack_name = "64bit Amazon Linux 2 v3.7.7 running Corretto 11"
 
   setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.eb_instance_profile.name
-  }
-
-  setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
@@ -24,12 +18,6 @@ resource "aws_elastic_beanstalk_environment" "fresco_backend_env" {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "FRESCO_ENV"
     value     = "prod"
-  }
-
-  setting {
-    namespace = "aws:ec2:instances"
-    name      = "InstanceTypes"
-    value     = "t3.micro,t3.small"
   }
 }
 
