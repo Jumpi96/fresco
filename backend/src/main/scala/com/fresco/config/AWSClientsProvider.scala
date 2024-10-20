84403e502e39c58df4cde3f4b4dd63c80838509e
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-10-20 08:37:42 +0200

    handling aws clients for deployment

--- backend/src/main/scala/com/fresco/config/AWSClientsProvider.scala
+++ backend/src/main/scala/com/fresco/config/AWSClientsProvider.scala
@@ -1,21 +1,28 @@
 package com.fresco.config
 
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.auth._
 import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.typesafe.config.Config
 
 object AWSClientsProvider {
+  private def getCredentialsProvider(awsConfig: Config): AWSCredentialsProvider = {
+    if (awsConfig.hasPath("accessKey") && awsConfig.hasPath("secretKey")) {
+      new AWSStaticCredentialsProvider(
+        new BasicAWSCredentials(awsConfig.getString("accessKey"), awsConfig.getString("secretKey"))
+      )
+    } else {
+      new DefaultAWSCredentialsProviderChain()
+    }
+  }
+
   def createDynamoDBClient(awsConfig: Config): AmazonDynamoDB =
     AmazonDynamoDBClientBuilder.standard()
-      .withCredentials(new AWSStaticCredentialsProvider(
-        new BasicAWSCredentials(awsConfig.getString("accessKey"), awsConfig.getString("secretKey"))))
+      .withCredentials(getCredentialsProvider(awsConfig))
       .build()
 
   def createS3PresignedUrlClient(awsConfig: Config): AmazonS3 =
     AmazonS3ClientBuilder.standard()
-      .withCredentials(new AWSStaticCredentialsProvider(
-        new BasicAWSCredentials(awsConfig.getString("accessKey"), awsConfig.getString("secretKey"))))
+      .withCredentials(getCredentialsProvider(awsConfig))
       .build()
-
 }
